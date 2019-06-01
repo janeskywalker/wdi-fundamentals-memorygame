f00955d49c480e52edc50559f432e189a94e6814
Jane Skywalker <janehuizeng@gmail.com>
2019-06-01 11:31:06 -0700

    pushing two flipped cards in cardsInPlay and console.log them out

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,6 +2,17 @@
 
 
 
-const cards = ["queen", "queen", "king", "king",]
+const cards = ["queen", "queen", "king", "king"]
+
+var cardsInPlay = []
+
+var cardOne = cards[0]
+var cardTwo = cards[1]
+
+cardsInPlay.push(cardOne)
+cardsInPlay.push(cardTwo)
+
 
 console.log("User flipped " + cardOne)
+console.log("User flipped " + cardTwo)
+
