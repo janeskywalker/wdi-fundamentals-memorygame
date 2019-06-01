becc73b14954ecbeef8e2a3fdd9918fc5aa9c1cb
Jane Skywalker <janehuizeng@gmail.com>
2019-06-01 11:34:20 -0700

    writing if...else statement and alert users the result

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -16,3 +16,11 @@ cardsInPlay.push(cardTwo)
 console.log("User flipped " + cardOne)
 console.log("User flipped " + cardTwo)
 
+
+if(cardsInPlay.length ===2) {
+	if(cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!")
+	} else {
+		alert("Sorry, try again")
+	}
+}
