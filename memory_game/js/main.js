52053f4faef9d0f7b5e0335c1b8061c3d9be7af3
Jane Skywalker <janehuizeng@gmail.com>
2019-06-03 16:12:50 -0700

    creating function flipCard

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -6,21 +6,25 @@ const cards = ["queen", "queen", "king", "king"]
 
 var cardsInPlay = []
 
-var cardOne = cards[0]
-var cardTwo = cards[1]
-
-cardsInPlay.push(cardOne)
-cardsInPlay.push(cardTwo)
 
 
-console.log("User flipped " + cardOne)
-console.log("User flipped " + cardTwo)
+var function flipCard(cardId) {
 
 
-if(cardsInPlay.length ===2) {
-	if(cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!")
-	} else {
-		alert("Sorry, try again")
+	cardsInPlay.push(cardOne)
+	cardsInPlay.push(cardTwo)
+
+
+	console.log("User flipped " + cardOne)
+	console.log("User flipped " + cardTwo)
+
+
+	if(cardsInPlay.length ===2) {
+		if(cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!")
+		} else {
+			alert("Sorry, try again")
+		}
 	}
+
 }
