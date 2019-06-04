993b697a940634117e2c746b986758ca46e0162b
Jane Skywalker <janehuizeng@gmail.com>
2019-06-04 15:16:51 -0700

    creating four objects to hld data for four cards

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,37 @@
 
 
 
+// array that store data of the cards
+//const cards = ["queen", "queen", "king", "king"]
+const cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	}, 
 
-const cards = ["queen", "queen", "king", "king"]
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	}, 
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	}, 
 
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}, 
+]
+
+// storing flipped cards
 var cardsInPlay = []
 
+// check for match of the two flipped cards in the cardsInPlay array  
 function checkForMatch() {
 
 	if(cardsInPlay.length ===2) {
@@ -17,7 +43,7 @@ function checkForMatch() {
 	}
 }
 
-
+// after user flip two cards, push them into the cardsInPlay array, call checkForMatch
 function flipCard(cardsId) {
 
 	console.log("User flipped " + cards[cardsId])
@@ -28,6 +54,7 @@ function flipCard(cardsId) {
 
 }
 
+// user flip cards, calling flipCard
 flipCard(3)
 flipCard(2)
 
